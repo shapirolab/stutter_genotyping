859d6695942ffa649f328d70b99bfb4786492c5c
DC Project <dcsoft@n101.mcl4.weizmann.ac.il>
2023-03-15 08:54:00 +0200

    minimal example of STR stutter genotyping

---
+++ sequencing/calling/multi_hists.py
@@ -0,0 +1,118 @@
+from sequencing.calling.hist import Histogram
+
+class MonoSimulatedHistogram(Histogram):
+    def __init__(self, ms_len, simulation_cycle, simulated_hist):
+        assert isinstance(ms_len, int)
+        assert isinstance(simulation_cycle, int)
+        self._ms_len = ms_len
+        self._sim_cyc = simulation_cycle
+        super(MonoSimulatedHistogram, self).__init__(simulated_hist)
+
+    def __repr__(self):
+        return '{}\n{}'.format(self.allele_frozenset, super().__repr__())
+
+    @property
+    def simulation_cycle(self):
+        return self._sim_cyc
+
+    @property
+    def ms_len(self):
+        return self._ms_len
+
+    @property
+    def allele_frozenset(self):
+        return frozenset([self.ms_len])
+
+    @property
+    def identity(self):
+        return self.allele_frozenset, self._sim_cyc
+
+
+class MultiSimulatedHistogram(Histogram):
+    def __init__(self, ms_lens, simulation_cycle, simulated_hist):
+        assert isinstance(ms_lens, frozenset)
+        assert isinstance(simulation_cycle, int)
+        self._ms_lens = ms_lens
+        self._sim_cyc = simulation_cycle
+        super(MultiSimulatedHistogram, self).__init__(simulated_hist)
+
+    def __repr__(self):
+        return '{}\n{}'.format(self.allele_frozenset, super().__repr__())
+
+    @property
+    def simulation_cycle(self):
+        return self._sim_cyc
+
+    @property
+    def ms_lens(self):
+        return self._ms_lens
+
+    @property
+    def allele_frozenset(self):
+        return self.ms_lens
+
+    @property
+    def identity(self):
+        return self.allele_frozenset, self._sim_cyc
+
+
+class ProportionalMultiSimulatedHistogram(Histogram):
+    def __init__(self, ms_lens_and_proportions, simulation_cycle, simulated_hist):
+        assert isinstance(ms_lens_and_proportions, frozenset)
+        assert isinstance(simulation_cycle, int)
+        self._ms_lens_and_proportions = ms_lens_and_proportions
+        self._sim_cyc = simulation_cycle
+        self._alleles_to_proportions = {a: p for a, p in ms_lens_and_proportions if p > 0}
+        assert sum(self._alleles_to_proportions.values()) == 1
+        super(ProportionalMultiSimulatedHistogram, self).__init__(simulated_hist)
+
+    def __repr__(self):
+        return '{}\n{}'.format(self.alleles_to_proportions, super().__repr__())
+
+    @property
+    def simulation_cycle(self):
+        return self._sim_cyc
+
+    @property
+    def alleles_to_proportions(self):
+        return self._alleles_to_proportions
+
+    @property
+    def ms_lens_and_proportions(self):
+        return self._alleles_to_proportions.items()
+
+    @property
+    def allele_frozenset(self):
+        return frozenset(self._alleles_to_proportions.keys())
+
+    @property
+    def identity(self):
+        return frozenset(self.ms_lens_and_proportions), self._sim_cyc
+
+
+
+class VecBiProportionalMultiSimulatedHistogram(ProportionalMultiSimulatedHistogram):
+    def __init__(self, a1, a2, p1, p2,
+                 simulation_cycle, simulated_hist, vh1, vh2):
+        assert isinstance(simulation_cycle, int)
+        ms_lens_and_proportions = (a1, p1), (a2, p2)
+        self._ms_lens_and_proportions = ms_lens_and_proportions
+        self._sim_cyc = simulation_cycle
+        self._alleles_to_proportions = {a: p for a, p in ms_lens_and_proportions if p > 0}
+        assert sum(self._alleles_to_proportions.values()) == 1
+        self._vh1 = vh1
+        self._vh2 = vh2
+        self._a1 = a1
+        self._a2 = a2
+        self._p1 = p1
+        self._p2 = p2
+
+        super(ProportionalMultiSimulatedHistogram, self).__init__(simulated_hist)
+
+    @property
+    def vh1(self):
+        return self._vh1
+
+    @property
+    def vh2(self):
+        return self._vh2
---
+++ utils.py
@@ -0,0 +1,72 @@
+from collections import namedtuple
+import numpy as np
+import pandas as pd
+
+def extract_normalized_vectors(df3_40):
+    vech_keys = []
+    vech_values = []
+    for k, r in bar(df3_40.iterrows()):
+        vech = np.array(r)
+        vech_normed = vech/np.linalg.norm(vech, ord=2)
+        if np.any(np.isnan(vech_normed)):
+            print('warning nans in {}'.format(k))
+            continue
+        vech_keys.append(k)
+        vech_values.append(vech_normed)
+    return vech_keys, vech_values
+
+Call = namedtuple('Call',['hist','conf'])
+def map_tree_query(ind, dist, vech_keys, hist_map):
+    called = {}
+    for (ind0, dist0, k) in bar(zip(ind, dist, vech_keys)):
+        if len(ind0)!=0:
+            imin = np.argmin(dist0)
+            called[k] = Call(hist_map[ind0[imin]], dist_to_cosing_score(dist0[imin]))
+    return called
+
+
+import lzma
+import pickle
+def load_kd_tree(kd_tree_pickle_path="ac_kdtree.pickle", hist_map="ac_hist_map.pickle"):
+
+    with lzma.open("kdtree.xz", "rb") as f:
+        kdtree = pickle.load(f)
+
+
+    with lzma.open("hist_map.xz", "rb") as f:
+        hist_map = pickle.load(f)
+    return kdtree, hist_map
+
+
+def cosine_score_to_dist(alpha):
+    return 2*np.sin(np.arccos(1-alpha)/2)
+
+def dist_to_cosing_score(d):
+    return 1-np.cos(2*np.arcsin(d/2))
+
+
+
+from frogress import bar
+from decimal import Decimal
+conf = 0.05
+CONF_RADIOS_THRESHOLD = cosine_score_to_dist(conf)
+
+def genotype_stutter_histograms(df):
+    print("extracting normalized vectors")
+    vech_keys, vech_values = extract_normalized_vectors(df)
+    print("loading reference histograms")
+    kdtree, hist_map = load_kd_tree()
+    print("querying kd-tree")
+    (ind, dist) = kdtree.query_radius(vech_values, r=CONF_RADIOS_THRESHOLD, return_distance=True)
+    print("mapping results")
+    called = map_tree_query(ind, dist, vech_keys, hist_map)
+    flat_called = dict()
+    for k, call_item in bar(called.items()):
+        alleles_and_proportions = sorted(call_item.hist.alleles_to_proportions.items())
+        if len(alleles_and_proportions) == 1:
+            alleles_and_proportions = alleles_and_proportions+[(0, Decimal('0.0000'))]
+        flat_called[k] = list(alleles_and_proportions[0])+list(alleles_and_proportions[1])+[call_item.conf, call_item.hist.simulation_cycle]
+    kd_calling_df = pd.DataFrame.from_dict(flat_called).T
+    kd_calling_df.index.set_names(df.index.names, inplace=True)
+    kd_calling_df.columns=['kd_a1', 'kd_prop1','kd_a2', 'kd_prop2', 'kd_conf', 'kd_cycles']
+    return kd_calling_df
